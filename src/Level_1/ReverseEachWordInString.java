04d62795e6f532fbd6a03bf820c7d9f1e25780c0
Amit Upadhyay <amitu2016@gmail.com>
2025-03-30 22:49:14 +0530

    commit

---
+++ src/Level_1/ReverseEachWordInString.java
@@ -0,0 +1,16 @@
+package Level_1;
+
+public class ReverseEachWordInString {
+
+    public static String reverseEachWord(String str){
+        
+        String[] strArray = str.split(" ");
+        
+        
+        
+        
+        return str;
+
+    }
+    
+}

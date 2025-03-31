03727a848ff4d7a30a102a8de71f0e512ec848b4
Amit Upadhyay <amitu2016@gmail.com>
2025-03-31 07:23:49 +0530

    Reverse Each Word in String

--- src/Level_1/ReverseEachWordInString.java
+++ src/Level_1/ReverseEachWordInString.java
@@ -6,11 +6,31 @@ public class ReverseEachWordInString {
         
         String[] strArray = str.split(" ");
         
-        
-        
-        
-        return str;
+        String ans = "";
 
+        for (int i = 0; i < strArray.length; i++) {
+            String currString = strArray[i];
+            char [] charArray = currString.toCharArray();
+            int left = 0;
+            int right = charArray.length - 1;
+
+            while (left < right) {
+                char temp = charArray[left];
+                charArray[left] = charArray[right];
+                charArray[right] = temp;
+                left++;
+                right--;
+            }
+
+            ans+=new String(charArray) + " ";
+
+        } 
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String str = "Spring Boot is awesome";
+        System.out.println(reverseEachWord(str));
     }
     
 }
